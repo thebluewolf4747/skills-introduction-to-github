bf61e79fc3e9c7c6298f42e4ca72d923924012bd
thebluewolf4747 <Rayyan_007@outlook.com>
2025-10-07 15:27:46 +0100

    Week 1 Lab Python File

---
+++ week1_lab.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+# Storing text (string)
+student_name = "Alex"
+
+# Storing an integer
+student_age = 20
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+
+# A simple calculation
+num1 = 15
+num2 = 10
+total = num1 + num2
+
+print("The sum is:", total)
